c267974ddfa249e07fe4a4a00f5dcf83a5485861
ayoub <ayoubnasri206@gmail.com>
2024-08-03 11:03:17 +0200

    aesthetic frames

--- contact page/script3.js
+++ contact page/script3.js
@@ -1,8 +1,9 @@
-function contact (event){
-    event.preventDeafault()
-    Swal.fire({
-        title: "Good job!",
-        text: "You clicked the button!",
-        icon: "success"
-      });
+function contact(event) {
+  event.preventDefault(); 
+  Swal.fire({
+      title: "Thank you!",
+      text: "You submitted successfully!",
+      icon: "success",
+      confirmButtonText: "OK"
+  });
 }
